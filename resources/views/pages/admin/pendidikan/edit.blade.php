83783328afab00787b032a21b066b3aec680e022
khoican <ibnukhoirulprasetyo@gmail.com>
2024-01-21 10:31:47 +0700

    style: fix styling view

--- resources/views/pages/admin/pendidikan/edit.blade.php
+++ resources/views/pages/admin/pendidikan/edit.blade.php
@@ -11,7 +11,7 @@
                 <input type="text" id="title" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
             </div>
             <div class="mb-6">
-                <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Gambar<span class="text-red-500 font-bold">*</span></label>
+                <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Gambar <span class="text-red-500 font-bold">*</span></label>
                 <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file" required>
                 <p class="mt-1 text-xs text-gray-500 dark:text-gray-300" id="file_input_help">Silahkan masukkan gambar dengan format JPG, JPEG, PNG, atau SVG</p>
             </div>
